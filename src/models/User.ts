b2ae89e00d336fa079cf39400eac7fd074966126
Youjin-cmd <y.kimiau@gmail.com>
2024-01-29 23:42:01 +0900

    Fix. modify User modelel regarding questions array

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
 const questionsSchema = new Schema({
-  questions: [
-    {
-      question: {
-        type: String,
-        required: true,
-      },
-      answer: {
-        type: String,
-        required: true,
-      },
-      topic: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  topic: {
+    type: String,
+    required: true,
+  },
 });
 
 const userSchema = new Schema({
